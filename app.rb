b05fc196d7d3dc9d2a8264a1f22d91e70d42c004
Chad Lengefeld <chadlengefeld@gmail.com>
2020-04-27 06:13:26 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -13,8 +13,9 @@ class App < Sinatra::Base
 
   post '/checkout' do
     @params = params[:item]
-    @session = session[:item] << @params
-    @session = session[:item] = @params
+    @session = session[:item]
+    # << @params
+    @session << @params
      binding.pry
     erb :index
   end
